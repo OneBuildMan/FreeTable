c91198647ca0c0676e5890f413fc87877579ee5a
OneBuildMan <deianandrei22@yahoo.com>
2023-05-02 20:55:37 +0300

    created routes

--- freetable/src/components/App.js
+++ freetable/src/components/App.js
@@ -2,19 +2,26 @@ import React from "react"
 import Signup from "./Signup"
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from "../contexts/AuthContext"
+import Home from "./Home"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <AuthProvider>
       <Container
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh"} }
       >
         <div className="w-100" style={{ maxWidth: '400px'}}>
-        <Signup />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/signup' element={<Signup/>} />
+              </Routes>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   )
 }
 
