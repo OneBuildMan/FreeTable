ed52109869db0d0519981b6c6b2b145932a5fc8d
OneBuildMan <deianandrei22@yahoo.com>
2023-06-13 04:40:58 +0300

    admin can see/delete restaurants

--- freetable/src/components/admin.js
+++ freetable/src/components/admin.js
@@ -9,14 +9,18 @@ import { useEffect, useState } from 'react'
 import { firestore } from '../firebase'
 
 export default function Dashboard() {
-    const [users, setUsers] = useState([]);
-    const [owners, setOwners] = useState([]);
-    const [bannedUsers, setBannedUsers] = useState([]);
+    const [users, setUsers] = useState([])
+    const [owners, setOwners] = useState([])
+    const [bannedUsers, setBannedUsers] = useState([])
+    const [restaurants, setRestaurants] = useState([])
 
     const fetchData = async () => {
         const userCollection = await firestore.collection('users').where("banned", "==", "no").get();
         setUsers(userCollection.docs.map(doc => ({ ...doc.data(), id: doc.id})).filter(user => user.role === 'user'))
         setOwners(userCollection.docs.map(doc => ({ ...doc.data(), id: doc.id})).filter(user => user.role === 'owner'))
+
+        const restaurantCollection = await firestore.collection('restaurants').get()
+        setRestaurants(restaurantCollection.docs.map(doc => ({ ...doc.data(), id: doc.id})))
 
         const bannedCollection = await firestore.collection('users').where("banned", "==", "yes").get();
         setBannedUsers(bannedCollection.docs.map(doc => ({ ...doc.data(), id: doc.id})))
@@ -45,6 +49,13 @@ export default function Dashboard() {
                 banned: 'no'
             })
 
+            fetchData();
+        }
+    }
+
+    const deleteRestaurant = async (resId) => {
+        if(window.confirm('Are you sure you want to delete this user?')){
+            await firestore.collection('restaurants').doc(resId).delete()
             fetchData();
         }
     }
@@ -105,6 +116,26 @@ export default function Dashboard() {
                 </tbody>
             </table>
             </div>
+            
+            <div className='admin-table'>
+            <h1>Restaurants</h1>
+            <table className='admin-table'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {restaurants.map(restaurant => (
+                        <tr key={restaurant.id}>
+                            <td>{restaurant.name}</td>
+                            <td><button onClick={() => deleteRestaurant(restaurant.id)}>Delete restaurant</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            </div>
 
             <div className='admin-table'>
             <h1>Banned users</h1>
@@ -129,6 +160,8 @@ export default function Dashboard() {
                 </tbody>
             </table>
             </div>
+
+            
         </div>
         </>
     )
