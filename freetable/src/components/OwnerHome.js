4189f5b03b6773aa97977cea1faca500d7c4a726
OneBuildMan <deianandrei22@yahoo.com>
2023-06-15 21:43:36 +0300

    fixed button bug

--- freetable/src/components/OwnerHome.js
+++ freetable/src/components/OwnerHome.js
@@ -286,6 +286,7 @@ export default function Dashboard() {
       setRestaurantModal(false)
       setNewRestaurantModal(false)
       setReportModal(false)
+      setProfileModal(false)
     }
 
     function addNewModal() {
